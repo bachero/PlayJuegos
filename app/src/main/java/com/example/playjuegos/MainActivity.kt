02975c7f0ca7cd672ca76f539904723cddf6839b
10197825 <bacheroj@gmail.com>
2020-09-28 20:41:36 +0200

    add toolbar

--- app/src/main/java/com/example/playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.playjuegos
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +19,30 @@ class MainActivity : AppCompatActivity() {
         val player = findViewById(R.id.newPlayer) as Button
         player.setOnClickListener { lanzarNewPlayer() }
 
+        val titulo = findViewById(R.id.titulo) as TextView
+        titulo.setTypeface(Typeface.createFromAsset(assets, "Chicken Quiche.ttf"))
+
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
     }
 
     fun lanzarNewPlayer(){
         val i = Intent(this, NewPlayer::class.java)
         startActivity(i)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            (R.id.action_search) -> {return true}
+            (R.id.action_add) -> {return true}
+            else -> {return super.onOptionsItemSelected(item)} }
     }
 
 }
--- app/src/main/java/com/example/playjuegos/NewPlayer.kt
+++ app/src/main/java/com/example/playjuegos/NewPlayer.kt
@@ -2,10 +2,29 @@ package com.example.playjuegos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class NewPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.newplayer)
+
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            (R.id.action_search) -> {return true}
+            (R.id.action_add) -> {return true}
+            else -> {return super.onOptionsItemSelected(item)} }
     }
 }
